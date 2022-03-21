e405d56cd30fad7f277e9e1cbc8148ed16fd9da6
Pierre-Louis BOIDIN <macpierre-louisboidin@MacBook-Pro-de-Pierre-Louis.local>
2022-03-21 09:20:16 +0100

    Update the change directory

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,5 +1,53 @@
 #include "../includes/minishell.h"
 
+static int	ft_go_to_dir(const char *dir)
+{
+	char	*tmp;
+	char	*aux;
+	char	*home;
+
+	aux = ft_substr(dir, 1, ft_strlen(dir) - 1);
+	home = ft_genv("HOME");
+	if (!home)
+	{
+		free(aux);
+		return (1);
+	}
+	aux = // Créer une fonction Réalloc qui va prendre en argmts aux, la taille de dir + la taille de home +1;
+	tmp = ft_strjoin(home, aux);
+	if (chdir(tmp) == -1)
+	{
+		ft_print_err(tmp);
+		free(home);
+		free(aux);
+		free(tmp);
+		return (1);
+	}
+	free(home);
+	free(aux);
+	free(tmp);
+	return (0);
+}
+
+void	ft_upd_env(t_env **env)
+{
+	char	*path;
+
+	path = malloc(sizeof(char) * (PATH_MAX + 1));
+	if (!path)
+		return ;
+	if (getcwd(path, PATH_MAX) == NULL)
+	{
+		free(path);
+		path = ft_genv("PWD");
+		if (path)
+			return ;
+	}
+	free((*env)->value);
+	(*env)->value = ft_strjoin("PWD=", path);
+	free(path);
+}
+
 static int	ft_go_to_pwd(t_env *env)
 {
 	char	*tmp;
@@ -22,8 +70,9 @@ static int	ft_go_to_pwd(t_env *env)
 		return (1);
 	}
 	free(old);
-
-
+	ft_set_old(env, pwd, tmp);
+	ft_env_loc(env);
+	return (0);
 }
 
 int	ft_ch_dir(char **dir)
@@ -41,7 +90,16 @@ int	ft_ch_dir(char **dir)
 	{
 		if (!dir[1] || !ft_strncmp(dir[1], "~\0", 2))
 		{
-			
+			if (ft_ret_home() == 1)
+				return (1);
+			return (0);
 		}
+		else if (ft_go_to_dir(dir[1]) == 1)
+			return (1);
 	}
+	else if (dir[1][0] != '\0' && chdir(dir[1] == -1))
+		return (ft_print_err(dir[1]));
+	else
+		ft_env_set(*g_info.env);
+	return (0);
 }
--- builtins/cd_utils.c
+++ builtins/cd_utils.c
@@ -1,5 +1,40 @@
 #include "../includes/minishell.h"
 
+int	ft_go_to_home(void)
+{
+	char	*home;
+	t_env	*env;
+
+	home = ft_genv("HOME");
+	env = *g_info.env;
+	if (!home || chdir(home) == -1)
+	{
+		ft_print_err(home);
+		free(home);
+		return (0);
+	}
+	ft_env_loc(env);
+	free(home);
+	return (0);
+}
+
+int	ft_env_loc(t_env *env)
+{
+	t_env	*aux;
+	
+	aux = env;
+	while (aux != NULL)
+	{
+		if (ft_strncmp("PWD=", (char *)aux->value, 4) == 0)
+		{
+			ft_upd_env(&aux);
+			return (0);
+		}
+		aux = aux->next;
+	}
+	return (1);
+}
+
 void	ft_set_val(t_env **env, const char *val, int c)
 {
 	if (c == 0)
@@ -11,6 +46,23 @@ void	ft_set_val(t_env **env, const char *val, int c)
 		ft_lstadd_back(g_info.env, ft_lstnew((void *) ft_strdup(val)));
 }
 
+int	ft_try_go_oldpwd(t_env **env)
+{
+	t_list	*aux;
+	
+	aux = *env;
+	if (!env)
+		return (1);
+	while(*env)
+	{
+		if (ft_strncmp("OLDPWD=", (char *)(*env)->value, 7) == 0)
+			return (0);
+		*env = (*env)->next;
+	}
+	*env = aux;
+	return (1);
+}
+
 int	ft_set_old(t_env *env, char *pwd, char *val)
 {
 	int		ret;
@@ -18,7 +70,7 @@ int	ft_set_old(t_env *env, char *pwd, char *val)
 
 	tmp = env;
 	pwd = ft_genv("PWD");
-	
+	ret = ft_try_go_oldpwd(&tmp);
 	if (tmp = NULL)
 	{
 		free(pwd);
