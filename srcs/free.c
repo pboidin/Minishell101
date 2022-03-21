a9c80b2a48fa4429ff7bf5f5c0fe90a06b1b95af
Brice DETUNE <bdetune@e2r3p13.clusters.42paris.fr>
2022-03-21 11:57:09 +0100

    Freeing better

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: bdetune <bdetune@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 14:19:29 by bdetune           #+#    #+#             */
-/*   Updated: 2022/03/21 11:36:49 by bdetune          ###   ########.fr       */
+/*   Updated: 2022/03/21 11:52:12 by bdetune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,7 +125,7 @@ void	free_cmd(t_cmd *cmd)
 	if (cmd->cmd_name)
 		free(cmd->cmd_name);
 	if (cmd->cmd)
-		free(cmd->cmd_name);
+		free(cmd->cmd);
 	free_redirect(cmd);
 }
 
