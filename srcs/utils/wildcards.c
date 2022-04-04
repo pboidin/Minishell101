d13758855c8ba358c9948568d74fae7862e8f974
Pierre-louis BOIDIN <piboidin@e2r5p18.clusters.42paris.fr>
2022-04-04 16:55:59 +0200

    wildcards in the game

--- srcs/utils/wildcards.c
+++ srcs/utils/wildcards.c
@@ -1,24 +1,73 @@
 #include <stdio.h>
 #include <dirent.h>
-  
-int main(void)
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+
+int	ft_strchr(char *str, char c)
 {
-    struct dirent *de;  // Pointer for directory entry
-  
-    // opendir() returns a pointer of DIR type. 
-    DIR *dr = opendir(".");
-  
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory
-    {
-        printf("Could not open current directory" );
-        return 0;
-    }
-  
-    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
-    // for readdir()
-    while ((de = readdir(dr)) != NULL)
-            printf("%s\n", de->d_name);
-  
-    closedir(dr);    
-    return 0;
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int main(int argc, char **argv)
+{
+	char	*cmd;
+	struct	dirent *de;
+	(void)	argc;
+	int		i;
+
+	DIR *dr = opendir(".");
+	i = 0;
+	if (dr == NULL)
+	{
+		printf("Could not open current directory");
+		return (0);
+	}
+	cmd = argv[1];
+	if (argv[1] == NULL)
+		return (0);
+	while (cmd[i])
+	{
+		write(1, &cmd[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	i = 0;
+	if (cmd == NULL)
+        return (0);
+	if (ft_strchr(argv[1], '*') == 1)
+	{
+		while ((de = readdir(dr)) != NULL)
+		{
+			while (argv[1][i] == '*')
+				i++;
+			if (ft_strcmp(argv[1]))
+			ft_putstr(de->d_name);
+		}
+    	closedir(dr);    
+    	return 0;
+	}
 }
