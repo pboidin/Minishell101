6cf37f6de30a85a0a9ef817e587e44cd9def46be
Brice DETUNE <bdetune@e3r10p19.clusters.42paris.fr>
2022-04-15 19:10:37 +0200

    Cleaning

--- srcs/execution/split_tab_var.c
+++ srcs/execution/split_tab_var.c
@@ -6,7 +6,7 @@
 /*   By: bdetune <bdetune@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 15:36:14 by bdetune           #+#    #+#             */
-/*   Updated: 2022/04/15 16:04:05 by bdetune          ###   ########.fr       */
+/*   Updated: 2022/04/15 19:09:21 by bdetune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,24 @@ void	free_new_tab(t_block **new_tab, size_t x, size_t y)
 		free(new_tab[y + x]);
 		x--;
 	}
+	free(new_tab[y]);
 	free(new_tab);
+}
+
+size_t	ins_vars(t_block **new_words_tab, size_t y, char **var, size_t nb_var)
+{
+	size_t	x;
+
+	x = 1;
+	while ((x + 1) < nb_var)
+	{
+		new_words_tab[y + x] = (t_block *)ft_calloc(2, sizeof(t_block));
+		if (!new_words_tab[y + x])
+			return (free_new_tab(new_words_tab, x, y), free_char_tab(var), 0);
+		new_words_tab[y + x][0].str = var[x];
+		x++;
+	}
+	return (x);
 }
 
 size_t	split_tab_var(t_block ***words_tab, size_t j, size_t i, char **var)
@@ -88,23 +105,15 @@ size_t	split_tab_var(t_block ***words_tab, size_t j, size_t i, char **var)
 	new_words_tab[y] = insert_back_t_block(words_tab[0][y], i, var[0]);
 	if (!new_words_tab[y])
 		return (free(new_words_tab), free_char_tab(var), 0);
-	x = 1;
-	while ((x + 1) < nb_var)
-	{
-		new_words_tab[y + x] = (t_block *)ft_calloc(2, sizeof(t_block));
-		if (!new_words_tab[y + x])
-			return (free_new_tab(new_words_tab, x, y), free_char_tab(var), 0);
-		new_words_tab[y + x][0].str = var[x];
-		x++;
-	}
+	x = ins_vars(new_words_tab, y, var, nb_var);
+	if (!x)
+		return (0);
 	new_words_tab[y + x] = insert_front_t_block(words_tab[0][y], i, var[x]);
 	if (!new_words_tab[y + x])
-		return (free(new_words_tab[y]), free_new_tab(new_words_tab, x, y), \
-				free_char_tab(var), 0);
+		return (free_new_tab(new_words_tab, x, y), free_char_tab(var), 0);
 	free((*words_tab)[j][i].str);
 	free((*words_tab)[j]);
 	free(*words_tab);
-	free(var);
 	*words_tab = new_words_tab;
-	return (nb_var);
+	return (free(var), nb_var);
 }
