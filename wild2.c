16fc8b41df4f071787eee9129aad4cbc04690a06
Pierre-louis BOIDIN <piboidin@e2r5p18.clusters.42paris.fr>
2022-04-22 17:48:20 +0200

    update wildcard

--- wild2.c
+++ wild2.c
@@ -1,49 +1,78 @@
-#include <libc.h>
+#include <string.h>
+#include <unistd.h>
 #include <dirent.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-/* Print all the dirs starting from <path> [maybe recursive]. */
-int print_dirs(const char *path, int recursive)
+typedef struct s_wild {
+	char			*path;
+	struct s_wild	*next;
+}	t_wild;
+
+void	ft_strcpy(char *dst, const char *src)
 {
-    struct dirent	*de;
-    DIR 			*dr;
-    size_t			path_len;
+	unsigned int	i;
+
+	i = 0;
+	while (*src)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+}
+
+t_wild	*ft_lstnew(void *content)
+{
+	t_wild	*new_el;
+
+	new_el = malloc(sizeof(t_wild));
+	if (!new_el)
+		return (NULL);
+	new_el->path = content;
+	new_el->next = NULL;
+	return (new_el);
+}
+
+int print_dirs(const char *path, int recursive, t_wild *list)
+{
+	struct dirent	*de;
+	DIR				*dr;
+	size_t			path_len;
 	struct stat		fstat;
 	char			full_name[_POSIX_PATH_MAX + 1];
 
-    if (!path) /* Check that input params */
+    if (!path)
         return -1;
     path_len = strlen(path);  
     if (!path || !path_len || (path_len > _POSIX_PATH_MAX))
         return -1;
-    dr = opendir(path); /* open directory */
+    dr = opendir(path);
     if (dr == NULL)
         return -1;
     while ((de = readdir(dr)) != NULL)
     {
-        /* Calculate full name, check we are in file length limts */
         if ((path_len + strlen(de->d_name) + 1) > _POSIX_PATH_MAX)
             continue;
-
         strcpy(full_name, path);
         if (full_name[path_len - 1] != '/')
             strcat(full_name, "/");
         strcat(full_name, de->d_name);
-
-        if ((strcmp(de->d_name, ".") == 0) || /* Ignore special directories */
-            (strcmp(de->d_name, "..") == 0))
+        if ((strcmp(de->d_name, ".") == 0) || (strcmp(de->d_name, "..") == 0))
             continue;
-
-        if (stat(full_name, &fstat) < 0) /* Print only if it is really directory */
+        if (stat(full_name, &fstat) < 0)
             continue;
-        if (S_ISDIR(fstat.st_mode))
+        if (S_ISDIR(fstat.st_mode) || S_ISREG(fstat.st_mode))
         {
-            printf("%s\n", full_name);
+			list->path = full_name;
+            printf("%s C'est ca\n", full_name);
             if (recursive)
-                print_dirs(full_name, 1);
+                print_dirs(full_name, 1, list);
         }
     }
-
-    /* Finalize resources. */
     (void)closedir(dr);
     return 0;
 }
@@ -51,9 +80,18 @@ int print_dirs(const char *path, int recursive)
 /* We are taking first argument as initial path name. */
 int main(int argc, const char* argv[])
 {
-    if (argc < 2)
-        return -1;
+	// char	**tab;
+	t_wild	*list;
 
-    print_dirs(argv[1], 1);
-    return 0;
+	list = ft_lstnew("NULL");
+	// tab = NULL;
+	if (argc != 2)
+		return (0);
+	print_dirs(argv[1], 1, list);
+	while (list->next)
+	{
+		printf("list : %s\n", list->path);
+		list = list->next;
+	}
+	return 0;
 }
